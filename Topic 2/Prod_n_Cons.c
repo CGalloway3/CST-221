2b98a8e9ab7327363ce591ec2a059e1f5799c965
Chad Galloway <galloway.ce@sbcglobal.net>
2018-12-20 03:49:35 -0600

    Topic 2 commit

---
+++ Topic 2/Prod_n_Cons.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define TRUE 1
+
+int theProduct;					
+_Bool bufferSize = 0;   // Size of buffer 0 or 1
+unsigned short WORD;  // Buffer used between producer and consumer 
+
+pthread_mutex_t the_mutex;
+pthread_cond_t consumer_waiting, producer_waiting; // used for signaling
+
+void put (unsigned short value) { // Puts the value passed, into the word and increases the buffer Size variable.
+	WORD = value;
+	bufferSize++;
+}
+
+int get() {  // decreases the buffer size variable and returns the word
+	bufferSize--;
+	return WORD;
+}
+
+int produce(){ // Produces the Product.
+	printf("Producing item %i \n", (unsigned short)++theProduct);
+	return theProduct;
+}
+
+void consume(unsigned short w){ // Comsumes the Product.
+	printf("Consuming item %i \n", w);
+}
+
+void *producer(void *ptr) // Entry point for the producer thread.
+{	
+	int w;
+	
+	while (TRUE) {
+		pthread_mutex_lock(&the_mutex);  // get exclusive access to buffer
+		while (bufferSize == 1) pthread_cond_wait(&producer_waiting, &the_mutex); // Buffer full wait
+		w = produce();
+		put(w);
+		pthread_cond_signal(&consumer_waiting); 	//wake up consumer
+		pthread_mutex_unlock(&the_mutex);  //release access to buffer
+	}
+	pthread_exit(0);
+}
+
+void *consumer(void *ptr) // Entry point for the comsumer thread.
+{	
+	int w;
+	
+	while (TRUE) {
+		pthread_mutex_lock(&the_mutex); //get exclusive access to buffer
+		while (bufferSize == 0 ) pthread_cond_wait(&consumer_waiting, &the_mutex);
+		w = get();  //take_item out of buffer
+		consume(w);				
+		pthread_cond_signal(&producer_waiting); 	//wake upproducer
+		pthread_mutex_unlock(&the_mutex);  //release access to buffer
+	}
+	pthread_exit(0);
+} 
+
+int main(int argc, char *argv[])
+{
+	pthread_t prod, cons;
+	pthread_mutex_init(&the_mutex, 0);
+	pthread_cond_init(&consumer_waiting, 0);
+	pthread_cond_init(&producer_waiting, 0);
+	pthread_create(&cons, 0, &consumer, 0);
+	pthread_create(&prod, 0, &producer, 0);
+	pthread_join(prod, 0);
+	pthread_join(cons, 0);
+	pthread_cond_destroy(&consumer_waiting);
+	pthread_cond_destroy(&producer_waiting);
+	pthread_mutex_destroy(&the_mutex);
+} 
